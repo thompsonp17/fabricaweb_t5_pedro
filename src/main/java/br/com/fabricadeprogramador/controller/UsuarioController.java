91c4ec7ba7acb152fbedcf9a9241023856f55840
thompsonp17 <thompsonp17@yahoo.com.br>
2016-01-19 17:32:06 -0200

    Atulização até a aula 4 @ 19:37. Continuar!

--- src/main/java/br/com/fabricadeprogramador/controller/UsuarioController.java
+++ src/main/java/br/com/fabricadeprogramador/controller/UsuarioController.java
@@ -8,29 +8,59 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/usercontroller")
+import br.com.fabricadeprogramador.entidade.Usuario;
+import br.com.fabricadeprogramador.persistencia.jdbc.UsuarioDAO;
+
+@WebServlet("/usercontroller.do")
 public class UsuarioController extends HttpServlet{
-	
+
 	public UsuarioController() {
 		System.out.println("Construtor...");
 	}
-	
+
 	@Override
 	public void init() throws ServletException {
 		System.out.println("Init...");
 		super.init();
 	}
-	
+
 	@Override
 	public void destroy() {
 		System.out.println("Destroy...");
 		super.destroy();
 	}
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		System.out.println("Ok!");
+		//PAUSE: video-aula 4: @ 19:37
+		
+		System.out.println("Sucesso!!!");
 	}
-	
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String id = req.getParameter("id");
+		String nome = req.getParameter("nome");
+		String login = req.getParameter("login");
+		String senha = req.getParameter("senha");
+
+		Usuario user = new Usuario();
+
+		if (id != null)
+			user.setId(Integer.parseInt(id));
+
+		user.setLogin(login);
+		user.setNome(nome);
+		user.setSenha(senha);
+
+		UsuarioDAO userDAO = new UsuarioDAO();
+		userDAO.salvar(user);
+
+		resp.getWriter().print("Sucesso! Salvou direitinho!");
+
+		System.out.println("Sucesso no doPost!");
+	}
+
 }
--- src/main/java/br/com/fabricadeprogramador/persistencia/jdbc/ConexaoFactory.java
+++ src/main/java/br/com/fabricadeprogramador/persistencia/jdbc/ConexaoFactory.java
@@ -9,9 +9,14 @@ public class ConexaoFactory {
 	public static Connection getConnection() {
 		
 		try {
+			
+			Class.forName("org.postgresql.Driver");
 			return DriverManager.getConnection("jdbc:postgresql://localhost:5432/fabricaweb","postgres","jEsus777");
 		} catch (SQLException e) {
 			// Relançando uma Runtime exception para o DAO não ficar dependente de uma SQL Exception
+			throw new RuntimeException(e);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
 			throw new RuntimeException(e);
 		}
 		
